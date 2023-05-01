ab818e2b4c73b3000b9bed7bf66c1278a77a2574
Katie Kew <ektr.klsh@gmail.com>
2023-05-01 22:43:16 +0300

    Final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,25 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
 
+
+function sendRequest(name, phone, address, goods, sum) {
+ 
+    let data = {order: {}, goods: []};
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+
+        let product = {};
+        product.title = goods[i].title;
+        product.count = goods[i].count;
+        data.goods.push(product);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
+    data.client = `${name} ${phone}`;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
